b9d6e98c0c14f49f236a3f28194414edeae8a8ad
dep <1649743146@qq.com>
2023-08-16 17:13:10 +0800

    feat: 生命周期validate

--- nuxt01-start/pages/index.vue
+++ nuxt01-start/pages/index.vue
@@ -9,6 +9,12 @@ export default {
   // middleware: 'auth' // 页面层级中间件定义
   middleware() {
     console.log('middleware->pages/index.vue')
+  },
+
+  // 参数的有效性
+  validate({params, query}) {
+    console.log('validate', params, query)
+    return true; // true页面才会显示
   }
 }
 </script>
