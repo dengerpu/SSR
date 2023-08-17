715c24ac32087be21e0f30d7f0e3968dd4dae7b1
dep <1649743146@qq.com>
2023-08-17 10:56:40 +0800

    feat: 生命周期asyncData和fetch

--- nuxt01-start/pages/index.vue
+++ nuxt01-start/pages/index.vue
@@ -15,6 +15,30 @@ export default {
   validate({params, query}) {
     console.log('validate', params, query)
     return true; // true页面才会显示
+  },
+
+  // 读数据，返回给组件
+  asyncData(context) {
+    // 异步业务逻辑，读取服务器数据
+    console.log('asyncData')
+    // 返回的数据会和data里面的数据合并
+    return {
+      b: 2
+    }
+  },
+
+  // 读数据，返回给vuex
+  fetch({store}) {
+    // 异步业务逻辑，读取服务器数据提交给vuex
+    console.log('fetch')
+  },
+
+
+  data() {
+    return {
+      a: 1,
+      b: 1111
+    }
   }
 }
 </script>
