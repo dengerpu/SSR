f22efcdabd5d2af982fd61b5d5bb977034c1aa58
dep <1649743146@qq.com>
2023-08-24 11:31:26 +0800

    feat: 新增自定义html模版

--- nuxt01-start/pages/index.vue
+++ nuxt01-start/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>首页</h1>
     <!-- <h3>{{title}}</h3> -->
     <el-button type="primary">按钮</el-button>
@@ -123,3 +123,6 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+</style>
